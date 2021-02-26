82dcaf896ffb2ce8a26a57310dead6f980eef702
Andrey Sheshukov <ash@jinr.ru>
2021-02-26 20:58:01 +0300

    Added basic test for LLR

--- sn_stat/test_llr.py
+++ sn_stat/test_llr.py
@@ -8,17 +8,6 @@ import pytest
 Tvalue = st.floats(-10000,10000)
 Trange = st.tuples(Tvalue, Tvalue).map(sorted)
 
-@given(Tvalue,Tvalue, Trange)
-def test_llr_const_single(t0,t_data,time_window):
-    with pytest.raises(ValueError):
-        llr = LLR(1,2)
-    
-    l = LLR(1,1,time_window=time_window)
-    if(time_window[0] <= t_data-t0 <= time_window[1]):
-        assert l(t_data,t0)==np.log(2)
-    else:
-        assert l(t_data,t0)==0
-
 Xval = st.floats(-10000,10000)
 Yval = st.floats(0,10000)
 
@@ -54,3 +43,19 @@ def test_distr_hist(d):
     vals,bins = d.histogram(d.bins)
     assert np.allclose(bins,d.bins)
     assert np.allclose(vals,d.vals)
+
+    vals = d.histogram([d.bins.min(),d.bins.max()])[0]
+    assert np.allclose(vals,[1])
+
+#test LLRs
+@given(Tvalue,Tvalue, Trange)
+def test_llr_const_single(t0,t_data,time_window):
+    with pytest.raises(ValueError):
+        llr = LLR(1,2)
+    
+    l = LLR(1,1,time_window=time_window)
+    if(time_window[0] <= t_data-t0 <= time_window[1]):
+        assert l(t_data,t0)==np.log(2)
+    else:
+        assert l(t_data,t0)==0
+
