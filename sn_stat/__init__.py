44507fb00172e8044ea31117df6b0a44cb26b362
Andrey Sheshukov <ash@jinr.ru>
2021-02-28 17:50:14 +0300

    Read version from module; Add extra dependencies for 'test' and 'doc'

--- sn_stat/__init__.py
+++ sn_stat/__init__.py
@@ -1,6 +1,9 @@
+__version__='0.2'
+
 from .rate import rate,log_rate
 from .sampler import Sampler
 from . import  llr, signals
 from .llr import LLR
 from .signals import Signal
 from .sig_calc import  ShapeAnalysis, z2p, p2z
+
