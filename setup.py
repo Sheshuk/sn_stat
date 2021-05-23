792dedb8f0e602582e75258cd0be1d04bb6f7503
Andrey Sheshukov <ash@jinr.ru>
2021-05-23 14:46:14 +0300

    distutils -> setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools 
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(name='sn-stat',
         version='0.3',
         description='Statistical methods for supernova neutrino detection',
+        long_description=readme,
+        long_description_content_type='text/markdown',
         author='Andrey Sheshukov',
         author_email='ash@jinr.ru',
         licence='GNU GPLv3',
         packages=['sn_stat'],
         install_requires=['numpy','scipy'],
         extras_require={'doc':['sphinx','sphinx-rtd-theme'],
-                        'test':['pytest','hypothesis','flake8']}
+                        'test':['pytest','hypothesis','flake8']},
+        python_requires='>=3.7'
      )
 
